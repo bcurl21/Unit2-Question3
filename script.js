fbf040eeeebbc3a55eaa5f7922458b20d849001a
bcurl21 <74270428+bcurl21@users.noreply.github.com>
2021-02-16 18:30:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,71 @@
-    require([
-      "esri/Map",
-      "esri/layers/CSVLayer",
-      "esri/views/MapView",
-      "esri/config",
-      "esri/core/urlUtils",
-      "dojo/domReady!"
-    ], function(
-      Map,
-      CSVLayer,
-      MapView,
-      esriConfig,
-      urlUtils
-    ) {
+ require(["esri/Map", "esri/layers/CSVLayer", "esri/views/MapView", "esri/widgets/Legend"], function(
+        Map,
+        CSVLayer,
+        MapView,
+        Legend
+      ) {
+        const url = "https://raw.githubusercontent.com/gbrunner/Advanced_Python_for_GIS_and_RS/master/Week%202/stl_crime_wgs_84.csv";
 
-     
-     var url = "https://raw.githubusercontent.com/gbrunner/Advanced_Python_for_GIS_and_RS/master/Week%202/stl_crime_wgs_84.csv";
-     esriConfig.request.corsEnabledServers.push('https://rawgit.com');
-
-     
+        // Paste the url into a browser's address bar to download and view the attributes
+        // in the CSV file. These attributes include:
+        // * mag - magnitude
+        // * type - earthquake or other event such as nuclear test
+        // * place - location of the event
+        // * time - the time of the event
 
         const template = {
-          title: "Earthquake Info",
-          content: "Magnitude {mag} {type} hit {place} on {time}."
+   title: "Crime committed at {ILEADSStreet}"
+};
+
+        // The heatmap renderer assigns each pixel in the view with
+        // an intensity value. The ratio of that intensity value
+        // to the maxPixel intensity is used to assign a color
+        // from the continuous color ramp in the colorStops property
+
+        const renderer = {
+          type: "heatmap",
+          colorStops: [
+            { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+            { color: "#2EFE2E", ratio: 0.083 },
+            { color: "#64FE2E", ratio: 0.166 },
+            { color: "#9AFE2E", ratio: 0.249 },
+            { color: "#C8FE2E", ratio: 0.332 },
+            { color: "#F7FE2E", ratio: 0.415 },
+            { color: "#FACC2E", ratio: 0.498 },
+            { color: "#FE9A2E", ratio: 0.581 },
+            { color: "#FE642E", ratio: 0.664 },
+            { color: "#FE2E2E", ratio: 0.747 }
+          ],
+          maxPixelIntensity: 25,
+          minPixelIntensity: 0
         };
 
-        const csvLayer = new CSVLayer({
-          url: url,
-          copyright: "USGS Earthquakes",
-          popupTemplate: template
+        const layer = new CSVLayer({
+        url: url,
+        title: "St. Louis Crime Heatmap",
+        copyright: "St. Louis Police Department",
+		latitudeField:"Lat",
+        longitudeField:"Lon",
+		popupTemplate: template,
+		renderer: renderer
+});
+
+        const map = new Map({
+          basemap: "gray-vector",
+          layers: [layer]
         });
 
-        var symbol = {
-          type: "simple-marker", 
-          color:"green"
-        };
+        const view = new MapView({
+          container: "viewDiv",
+          center: [-90.2,38.63],
+          zoom: 12,
+          map: map
+        });
 
-      csvLayer.renderer = {
-        type: "simple", // autocasts as new SimpleRenderer()
-        symbol: symbol
-      };
-
-      var map = new Map({
-        basemap: "gray",
-        layers: [csvLayer]
+        view.ui.add(
+          new Legend({
+            view: view
+          }),
+          "bottom-right"
+        );
       });
-
-      var view = new MapView({
-        container: "viewDiv",
-        center: [-90.2, 38.63],
-        zoom: 12,
-        map: map
-      });
-
-    });
